2d30122fa8f4ef0096e4e006da2157af94a301a2
Zerowaltz <xiaochi_zhang@brown.edu>
2022-11-28 16:10:31 -0500

    fin

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Development
 
 ### Link to Deployed Website
-If you used the stencil code, this is `https://<your GitHub username>.github.io/<name of your repository>`
+If you used the stencil code, this is `https://sanejellyfish742.github.io/development`
 
 ### Goal and Value of the Application
-
+The application provides a list of available rooms in the CIT, information about each room like capacity and equipment and a way to reserve them. Users decide which rooms they need at a glance as well as filter by several criteria.
 ### Usability Principles Considered
-
+The rooms are each in a flexbox that is responsive to the size of the browser window. Within each box, the hierarchy displays the most important information in larger font. Each image has an alt text with the description of the room. The buttons are clearly labeled with their functions.
 ### Organization of Components
-
+Each button is a component that has an associated onclick function, and the aggregator is also a component that takes in the list of selected rooms as a prop. Whenever the prop is modified, the aggregator will re-render the list and its members.
 ### How Data is Passed Down Through Components
-
+Each room object has an associated description, capacity, and flag indicating whether they have computers/AV equipment in the json file. The list of selected rooms is a set that has room objects as its elements. Information like capacity and equipment are accessed through the individual room objects.
 ### How the User Triggers State Changes
-
+When the user presses a button to add or remove a room, it updates the states of the list of rooms chosen and the total capacity, which is accessible in the whole document. When the user presses a button to sort, the state of the list changes to one of three pre-sorted arrays of rooms. There are also checkboxes for filtering, which flip state variables that determine which rooms are shown.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,155 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import bakeryData from "./assets/bakery-data.json";
 
+
+
+let nextId = 0;
 function App() {
+	const [capacity, setCapacity] = useState(0);
+	const [computers, setComputers] = useState(false);
+	const [av, setAV] = useState(false);
+	const [rooms, setRooms] = useState(() => new Set());
+	const base = bakeryData
+    const numAscending = [...bakeryData].sort((a, b) => a.capacity - b.capacity)
+    const numDescending = [...bakeryData].sort((a, b) => b.capacity - a.capacity)
+	const [sort, setSort] = useState(base);
+
+	const handleComputers = () => {
+	setComputers(!computers);	};
+	const handleAV = () => {
+	setAV(!av);	};
+
+function AddButton({ room, onClick }) {
+  return (
+    <button onClick={() => {
+				if(!rooms.has(room)) {
+        setCapacity(a => a + room.capacity);
+		setRooms(prev => new Set(prev).add(room));
+	} else {
+		
+	}}}>Add to Reservation</button>
+  );
+}
+
+function RemoveButton({ room, onClick }) {
+  return (
+    <button onClick={() => {
+		
+
+		if(rooms.has(room)) {
+        setCapacity(a => a - room.capacity);
+        setRooms(prev => {
+      const next = new Set(prev);
+
+      next.delete(room);
+
+      return next;
+    });
+	} else {
+		
+	}}}>Remove from Reservation</button>
+  );
+}
+
+
+function ResetButton({ onClick }) {
+  return (
+    <button onClick={() => {
+        setCapacity(0);
+        setRooms(() => new Set());
+      }}>Clear Selections</button>
+  );
+}
+
+
+function SortUpButton({ onClick }) {
+  return (
+    <button onClick={() => {
+        setSort(numAscending);
+      }}>Sort by Ascending Capacity</button>
+  );
+}
+
+function SortDownButton({ onClick }) {
+  return (
+    <button onClick={() => {
+        setSort(numDescending);
+      }}>Sort by Descending Capacity</button>
+  );
+}
+
+function ResetList({ onClick }) {
+  return (
+    <button onClick={() => {
+        setSort(base);
+      }}>Sort by Room Number (Original Order)</button>
+  );
+}
+
+class Aggregator extends React.Component {
+  render() {
+	  return (
+  <div><ol>
+		{!capacity && <p>No rooms selected!</p>}
+        {Array.from(this.props.rooms).map(room => (
+          <li>{room.name}, capacity: {room.capacity}</li>
+        ))}
+      </ol>
+	  Total capacity of rooms in reservation: {capacity}</div>);
+}
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	<label>
+        <input
+          type="checkbox"
+          checked={computers}
+          onChange={handleComputers}
+        />
+        Has computers?
+      </label>
+	  	<label>
+        <input
+          type="checkbox"
+          checked={av}
+          onChange={handleAV}
+        />
+        Has AV?
+      </label>
+	  <br>
+	  </br>
+	  <SortUpButton onclick={SortUpButton}/>
+	  <SortDownButton onclick={SortDownButton}/>
+	  <ResetList onclick={ResetList}/>
+      <h1>Reservable CIT Rooms</h1> {/* TODO: personalize your bakery (if you want) */}
+      <div class="flex"> {sort.map((room, index) =>  
+
+		(!computers && !av) || (!computers && av && room.av) || (computers && !av && room.computers) || (computers && av && room.computers && room.av) ? 
+		(<div class="item"><p>
+		
+	      <h2>{room.name}
+		  	      <p>Capacity: {room.capacity}</p></h2>
+				  	     <p> {room.description}</p>
+				  	     <p>Has computers: {room.computers}</p>
+				  	     <p>Has AV: {room.av}</p>
+
+					   <p> <img style={{ width: "70%", height: "50%" }} src={room.image} alt={room.description}></img></p>
+				  	   <p>  <AddButton room={room} onclick={AddButton}/></p>
+				  	   <p>  <RemoveButton room={room} onclick={RemoveButton}/></p>
+		</p></div>) : (<></>) 
+      )}
+	  </div>
+        <h2>Current Reservation</h2>
+        {/* TODO: render a list of items in the cart */}
+		<Aggregator rooms={rooms}>
+		</Aggregator>
+				  	   <p>  <ResetButton onclick={ResetButton}/></p>
+					   
+					   <button>Reserve!</button>
+
     </div>
   );
 }
